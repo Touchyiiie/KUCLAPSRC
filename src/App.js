5a6cc4ef275bff3ba927db4842f5e6e29dc6622e
Nattapol Prairuenrom <loukfour@gmail.com>
2024-04-01 20:21:30 +0700

    finnish front end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,92 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import coursesData from './courses.txt';
+import subjectgroup from './course.txt';
+import one from './1.txt';
+import two from './2.txt';
+import three from  './3.txt';
+import four from './4.txt';
+import five from './5.txt';
 import Comment from './addandcomment.js'; // Import the Comment component
+import styled from 'styled-components';
+
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 30px;
+  text-align: center;
+  margin-bottom: -3px;
+  margin-top: -10px;
+  font-family: 'Roboto', sans-serif;
+`;
+
+const StyledH2 = styled.h2`
+  font-size: 18px;
+  text-align: center;
+  margin-top: 10px;
+  font-family: "Times New Roman", Times, serif;
+`;
+
+const StyledH3 = styled.h3`
+  font-size: 16px;
+  text-align: center;
+  margin-bottom: -10px;
+  margin-top: 5px;
+  margin-right: 560px;
+  color: gray;
+  text-decoration: underline;
+`;
+
+
+const StyledImage = styled.img`
+  width: 150px; /* ปรับขนาดของรูปภาพเป็น 150px */
+  margin-top: 20px; /* ขยับรูปภาพขึ้นจากขอบด้านบน */
+  margin-bottom: 20px; /* ขยับรูปภาพลงจากขอบด้านล่าง */
+  object-fit: contain; /* จัดการขนาดรูปภาพให้พอดีกับพื้นที่ของตัวเองโดยไม่เปลี่ยนรูปร่าง */
+`;
+
+const StyledSelectContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px; /* ปรับระยะห่างระหว่าง Dropdown กับส่วนอื่น ๆ ของเนื้อหา */
+`;
+
+const StyledSelect = styled.select`
+  width: 500px; /* ปรับความยาวของ dropdown */
+  font-size: 16px; /* ปรับขนาดตัวอักษรใน dropdown */
+  padding: 8px; /* ปรับระยะห่างรอบขอบของ dropdown */
+  border-radius: 15px; /* กำหนดความโค้งของ dropdown */
+`;
+
+const StyledSeparator = styled.div`
+  width: 100%;
+  max-width: 800px;
+  height: 3px;
+  border-top: 3px solid #000;
+  margin: 10px; 
+`;
 
 const HomePage = () => {
   const [selectedCourse, setSelectedCourse] = useState("");
   const [courses, setCourses] = useState([]);
-  const [coursesId, setCoursesId] = useState([]);
   const [idd, setId] = useState("");
+  const [subject, setsubject] = useState(1);
+  const [selectedGroupSubject, setSelectedGroupSubject] = useState("");
+  const [groupSubjects, setGroupSubjects] = useState([]);
+  const [checkstart,setcheckstart]=useState(false);
+
+  useEffect(() => {
+    fetchGroupSubjects();
+  }, []);
+
+  useEffect(() => {
+    fetchCourses();
+  }, [selectedGroupSubject]);
 
   useEffect(() => {
     fetchCourses();
@@ -17,43 +96,104 @@ const HomePage = () => {
     findIdWhenSelect(selectedCourse);
   }, [selectedCourse]);
 
-  const fetchCourses = async () => {
-    try {
-      const response = await axios.get(coursesData);
+  
+
+
+  const fetchCourses = async () => {  
+    if(subject==1){
+      const response = await axios.get(one);
       const courseList = response.data.split('\n').filter(course => course.trim() !== '');
-      const courseIds = response.data.split('\n').map(course => course.trim().split(' ')[0]);
-      setCoursesId(courseIds);
       setCourses(courseList);
-    } catch (error) {
-      console.error('Error fetching courses:', error);
-    }
+    } if(subject==2){
+      const response = await axios.get(two);
+      const courseList = response.data.split('\n').filter(course => course.trim() !== '');
+      setCourses(courseList);
+    } if(subject==3){
+      const response = await axios.get(three);
+      const courseList = response.data.split('\n').filter(course => course.trim() !== '');
+      setCourses(courseList);
+    } if(subject==4){
+      const response = await axios.get(four);
+      const courseList = response.data.split('\n').filter(course => course.trim() !== '');
+      setCourses(courseList);
+    } if(subject==5){
+      const response = await axios.get(five);
+      const courseList = response.data.split('\n').filter(course => course.trim() !== '');
+      setCourses(courseList);
+    } 
   };
 
   const handleCourseSelect = (event) => {
     setSelectedCourse(event.target.value);
   };
 
+  const fetchGroupSubjects = async () => {
+    try {
+      const response = await axios.get(subjectgroup);
+      const groupSubjectsData = response.data.split('\n').filter(course => course.trim() !== '');
+      setGroupSubjects(groupSubjectsData);
+    } catch (error) {
+      console.error('Error fetching group subjects:', error);
+    }
+  };
+  
+  const handleGroupSubjectSelect = (event) => {
+    setcheckstart(true);
+    setSelectedGroupSubject(event.target.value);
+    findIdWhenSelectgroupsubject(event.target.value);
+  };
+  
+
   const findIdWhenSelect = (selectedCourse) => {
     const courseId = selectedCourse.split(' ')[0]; // Get the course ID from the selected course string
     setId(courseId);
+    fetchCourses();
+  };
+
+  const findIdWhenSelectgroupsubject = (selectedGroupSubject) => {
+    const courseId = selectedGroupSubject.split(' ')[0]; // Get the course ID from the selected course string
+    setsubject(courseId);
+    fetchCourses();
   };
 
   return (
     <>
-      <h3>React Page</h3>
-      <label htmlFor="courseSelect">Select a Course:</label>
-      <select id="courseSelect" onChange={handleCourseSelect} value={selectedCourse}>
-        <option value="">-- Please Select --</option>
-        {courses.map((course, index) => (
-          <option key={index} value={course.trim()}>
-            {course.trim()}
-          </option>
-        ))}
-      </select>
-      <h3>you select {idd}</h3>
-      <Comment id_course={idd} />
+      
+        <StyledContainer>
+          <StyledImage src={require('./assets/logo.png')} alt="Logo" />
+          <StyledH1>KU CLAP SRC</StyledH1>
+          <StyledH2>เลือกหมวดหมู่วิชาเสรี</StyledH2>
+          <StyledSelect onChange={handleGroupSubjectSelect} value={selectedGroupSubject}>
+            <option value="">-- Please Select --</option>
+            {groupSubjects.map((groupSubject, index) => (
+              <option key={index} value={groupSubject.trim()}>
+                {groupSubject.trim()}
+              </option>
+            ))}
+          </StyledSelect>
+        </StyledContainer>
+        {!checkstart ? (<></>) : (
+        <StyledContainer>
+          <StyledH2>เลือกรายวิชา</StyledH2>
+          <StyledSelectContainer>
+            <StyledSelect onChange={handleCourseSelect} value={selectedCourse}>
+              <option value="">-- Please Select --</option>
+              {courses.map((course, index) => (
+                <option key={index} value={course.trim()}>
+                  {course.trim()}
+                </option>
+              ))}
+            </StyledSelect>
+          </StyledSelectContainer>
+          <StyledSeparator /> 
+          {selectedCourse && <StyledH2>รายละเอียด {selectedCourse}</StyledH2>}
+          <Comment id_course={idd} />
+        </StyledContainer>
+      )}
     </>
   );
+  
+
 };
 
 export default HomePage;
--- src/addandcomment.js
+++ src/addandcomment.js
@@ -1,7 +1,184 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
+
+const StyledH2 = styled.h2`
+  font-size: 18px;
+  text-align: center;
+  margin-top: 10px;
+  font-family: "Times New Roman", Times, serif;
+  text-align: left;
+`;
+
+const StyledButton = styled.button`
+  margin-top: 10px;
+  margin-bottom: 10px;
+  opacity: ${({ isActive }) => (isActive ? "1" : "0.5")};
+  border: none;
+  font-size: 28px;
+  background-color: transparent;
+  margin-right: 20px;
+  margin-left: 20px;
+  position: relative;
+  // border-bottom: ${({ isActive }) => (isActive ? "2px solid #000" : "")};
+  // border-bottom-width: ${({ isActive }) => (isActive ? "3px" : "0px")};
+  font-weight: ${({ isActive }) => (isActive ? "bold" : "normal")};
+  &:hover {
+    background-color: #2c7a7b;
+    border-radius: 25px;
+  }
+`;
+
+const ButtonSeparator = styled.div`
+  position: absolute;
+  width: 3px;
+  height: 100%;
+  background-color: #000;
+  right: calc(52.5%);
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  position: relative;
+  border-bottom: "3px solid #000";
+  
+`;
+
+const StyledList = styled.ul`
+  width: 750px;
+  border-radius: 15px;
+  border: 2px solid #000;
+  padding: 10px;
+  overflow: auto;
+`;
+
+const CommentForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const CommentLabel = styled.label`
+  font-size: 20px;
+  margin-bottom: 5px;
+`;
+
+const CommentTextArea = styled.textarea`
+  width: 20%-95%;
+  height: 100px;
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 10px; /* ปรับขนาดของขอบเส้นมุม */
+  border: 2px solid #ccc; /* ปรับสีและขนาดของเส้นขอบ */
+  resize: none; /* ป้องกันการปรับขนาด textarea โดยผู้ใช้งาน */
+  margin-bottom: 10px;
+`;
+
+
+const UserTextArea = styled.textarea`
+  width: 70%;
+  height: 15px;
+  font-size: 15px;
+  padding: 5px;
+  border-radius: 10px; /* ปรับขนาดของขอบเส้นมุม */
+  border: 2px solid #ccc; /* ปรับสีและขนาดของเส้นขอบ */
+  resize: none; /* ป้องกันการปรับขนาด textarea โดยผู้ใช้งาน */
+`;
+
+const StyledListItem = styled.li`
+  padding: 20px;
+  border: 3px solid #000;
+  list-style: none;
+  border-radius: 22px;
+  margin-bottom: 22px;
+`;
+
+const StyledListbet = styled.li`
+  display: flex;
+  justify-content: space-between;
+  border-radius: 10px;
+  margin-top: -5px;
+  margin-bottom: -10px;
+`;
+
+const StyledListonlybet = styled.li`
+  justify-content: space-between;
+  display: flex;
+  width: 38%;
+`;
+
+const StyledSeparator = styled.div`
+  width: 97%;
+  max-width: 800px;
+  height: 3px;
+  border-radius: 20%;
+  margin: 10px;
+  background-color: gray; 
+`;
+
+const UserInfo = styled.div`
+  font-weight: bold;
+  font-size: 15px;
+  color: gray;
+`;
+
+const BoldText = styled.div`
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const LikesBarContainer = styled.div`
+  width: 100%;
+  max-width: 500px;
+  height: 25px;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  margin-top: 10px;
+  margin-right: 5px;
+  margin-bottom: 5px;
+  border-radius: 15px; 
+`;
+
+const LikesBarFiller = styled.div`
+  height: 100%;
+  width: ${({ fillerWidth }) => fillerWidth}%;
+  background-color: #3cba9f;
+  border-radius: inherit;
+  text-align: right;
+`;
+
+const LikesBarContent = styled.span`
+  position: right;
+  color: #00000;
+`;
+
+const AddCommentButton = styled.button`
+  font-size: 20px;
+  padding: 10px 20px;
+  background-color: #3cba9f;
+  color: #ffffff;
+  border: none;
+  border-radius: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #2c7a7b;
+  }
+`;
+
 
 export default function Comment({ id_course }) {
+    
     const [comments, setComments] = useState([]);
     const [newComment, setNewComment] = useState({
         user: "",
@@ -9,14 +186,14 @@ export default function Comment({ id_course }) {
         comment: "",
         like: ""
     });
-    const [TotalLikesprecent, setTotalLikesprecent] = useState(0); // State to hold total likes
+    const [TotalLikesPercent, setTotalLikesPercent] = useState(0);
     const [counterpeople, setCounterpeople] = useState(0);
-    const [whobutton, setWhobutton] = useState(false); // Initialize with false
+    const [whobutton, setWhobutton] = useState(false);
     const [idc, setIdc] = useState("");
 
     useEffect(() => {
         fetchComments();
-    }, [id_course]); // Fetch comments whenever the course ID changes
+    }, [id_course]);
 
     useEffect(() => {
         calculateTotalLikes();
@@ -27,7 +204,7 @@ export default function Comment({ id_course }) {
     }, [id_course]);
 
     const fetchComments = () => {
-        axios.get(`http://localhost:5000/comments/${id_course}`) // Fetch comments for the specified course ID
+        axios.get(`http://localhost:5000/comments/${id_course}`)
             .then(response => setComments(response.data.comments))
             .catch(error => console.error('Error fetching comments:', error));
     };
@@ -37,15 +214,24 @@ export default function Comment({ id_course }) {
         for (const comment of comments) {
             likes += comment.like;
         }
-        const countpeople = comments.length;
-        const precent = (likes / countpeople * 100 / 5).toFixed(2); // Round to two decimal places
-        setCounterpeople(countpeople);
-        setTotalLikesprecent(precent);
+        if(likes === 0) {
+          setTotalLikesPercent(0);
+          setCounterpeople(0);
+        } else {
+          const countpeople = comments.length;
+          const percent = (likes / (countpeople * 5)) * 100; // Calculate percentage
+          setCounterpeople(countpeople);
+          setTotalLikesPercent(percent);
+        }
     };
     
 
-    const seeoradd = () => {
-        setWhobutton(!whobutton); // Toggle the button state
+    const seeoradd = (buttonType) => {
+        if (buttonType === "see") {
+            setWhobutton(false);
+        } else if (buttonType === "add") {
+            setWhobutton(true);
+        }
     };
 
     const handleInputChange = (e) => {
@@ -56,7 +242,7 @@ export default function Comment({ id_course }) {
     const handleAddComment = () => {
         axios.post('http://localhost:5000/comments', {
             ...newComment,
-            course: idc // Use idc as the value for the course field
+            course: idc
         })
         .then(() => {
             setNewComment({
@@ -72,62 +258,84 @@ export default function Comment({ id_course }) {
     
 
     const handleLikeChange = (e) => {
-        setNewComment({ ...newComment, like: parseInt(e.target.value) }); // Convert string to integer
+        setNewComment({ ...newComment, like: parseInt(e.target.value) });
     };
-
+    
     return (
-        <div>
-            <button type="button" onClick={seeoradd}>
-                {whobutton ? 'Add Comment' : 'See Comment'}
-            </button>
+        <>
+          {idc === "" ? (
+            <StyledH2>โปรดเลือกวิชา</StyledH2>
+          ) : (
+            <>
+            <StyledListonlybet>
             
-            {whobutton ? (
-                <>
-                    <h2>{'Add New Comment'}</h2>
-                    <form>
-                        <label>User:</label>
-                        <input type="text" name="user" value={newComment.user} onChange={handleInputChange} />
-                        <br/>
-                        <label>Comment:</label>
-                        <input type="text" name="comment" value={newComment.comment} onChange={handleInputChange} />
-                        <br/>
-                        <label>Like:</label>
+            <StyledH2>
+                คนชื่นชอบ :
+                </StyledH2>
+              <LikesBarContainer>
+              <LikesBarFiller fillerWidth={TotalLikesPercent}>
+                        <LikesBarContent>{TotalLikesPercent.toFixed(0)} % ฺ</LikesBarContent>
+                      </LikesBarFiller>
+                  </LikesBarContainer>
+                  </StyledListonlybet>
+            <StyledContainer>
+            <ButtonGroup isActive={whobutton}>
+              <StyledButton type="button" onClick={() => seeoradd("see")} isActive={!whobutton}>ดูความคิดเห็น</StyledButton>
+              <ButtonSeparator/>
+              <StyledButton type="button" onClick={() => seeoradd("add")} isActive={whobutton}>เพิ่มความคิดเห็น</StyledButton>
+            </ButtonGroup>
+                {whobutton ? (
+                  <>
+                  
+                  <StyledList>
+                      <CommentForm>
+                        <CommentLabel>ความคิดเห็น</CommentLabel>
+                        <CommentTextArea name="comment" value={newComment.comment} onChange={handleInputChange} />
+                        <CommentLabel>โดย</CommentLabel>
+                        <UserTextArea name="user" value={newComment.user} onChange={handleInputChange} />
+                        <CommentLabel>ความประทับใจ</CommentLabel>
                         <div>
-                            {[1, 2, 3, 4, 5].map((num) => (
-                                <label key={num}>
-                                    <input type="radio" name="like" value={num} checked={parseInt(newComment.like) === num} onChange={handleLikeChange} />
-                                    {num}
-                                </label>
-                            ))}
+                          {[1, 2, 3, 4, 5].map((num) => (
+                            <label key={num} style={{ fontSize: "20px", marginRight: "10px" }}>
+                              <input type="radio" name="like" value={num} checked={parseInt(newComment.like) === num} onChange={handleLikeChange} />
+                              {"★".repeat(num)}
+                            </label>
+                          ))}
                         </div>
                         <br/>
-                        <button type="button" onClick={handleAddComment}>
-                            {'Add Comment'}
-                        </button>
-                    </form>
-                </>
-            ) : (
-                <>
+                        <AddCommentButton type="button" onClick={handleAddComment}>
+                          Add Comment
+                        </AddCommentButton>
+                      </CommentForm>
+                    </StyledList>
+                  </>
+                ) : (
+                  <>
                     {comments.length === 0 ? (
-                        <p>No comments</p>
+                      <p>No comments</p>
                     ) : (
-                        <>
-                            <h1>Likes: {TotalLikesprecent}% from {counterpeople} people</h1> {/* Display total likes */}
-                            <h1>All Comments</h1>
-                            <ul>
-                                {comments.map(comment => (
-                                    <li key={comment._id}>
-                                        <div>User: {comment.user}</div>
-                                        <div>Course: {comment.course}</div>
-                                        <div>Comment: {comment.comment}</div>
-                                        <div>Like: {comment.like}</div>
-                                    </li>
-                                ))}
-                            </ul>
-                        </>
+                      <StyledList>
+
+                        <StyledH2 >ความคิดเห็นทั้งหมด {counterpeople} ความคิดเห็น</StyledH2 >
+                          {comments.map(comment => (
+                            
+                            <StyledListItem key={comment._id}>
+                              <BoldText>{comment.comment}</BoldText>
+                              <StyledSeparator/>
+                              <StyledListbet>
+                              <UserInfo>โดย {comment.user}</UserInfo>
+                              <UserInfo>ให้คะแนน {comment.like} / 5</UserInfo>
+                              </StyledListbet>
+                            </StyledListItem>
+                          ))}
+                      </StyledList>
                     )}
-                </>
-            )}
-        </div>
-    );
+                  </>
+                )}
+              </StyledContainer>
+              
+            </>
+          )}
+        </>
+      );
 }
